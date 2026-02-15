d54b2323afa5418f4aa4c44a03ceb09de8896f39
Amal k <119653788+Amalkxx02@users.noreply.github.com>
2026-02-15 21:11:17 +0530

    new apis

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -3,30 +3,43 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas.auth import AuthIn
-from app.security.jwt.dependency import get_current_user
+from app.security.jwt.dependency import get_current_user, get_refresh
 from app.services.auth import auth_service
 from app.database.session import get_db
 
 router = APIRouter()
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/sign_up", status_code=status.HTTP_201_CREATED)
 async def sign_up(
     user_in: AuthIn,
     db: AsyncSession = Depends(get_db),
 ):
-    """Create a new user"""
     try:
-        return await auth_service.insert(db, user_in.model_dump())
-        
+        return await auth_service.register(db, user_in.model_dump())
     except ValueError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e),
         )
-    
-@router.get("/verify_email",status_code=status.HTTP_200_OK,response_class=HTMLResponse)
-async def verify(token:UUID,db: AsyncSession = Depends(get_db)):
+
+
+@router.post("/sign_in", status_code=status.HTTP_200_OK)
+async def sign_in(
+    user_in: AuthIn,
+    db: AsyncSession = Depends(get_db),
+):
+    try:
+        return await auth_service.authenticate(db, user_in.model_dump())
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
+
+
+@router.get("/verify", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
+async def verify(token: UUID, db: AsyncSession = Depends(get_db)):
     await auth_service.verify(db, token)
     html_content = """
     <html>
@@ -43,3 +56,11 @@ async def verify(token:UUID,db: AsyncSession = Depends(get_db)):
     </html>
     """
     return HTMLResponse(content=html_content)
+
+@router.get("/sign_out",status_code=status.HTTP_200_OK)
+async def signout(db: AsyncSession = Depends(get_db),user_id:UUID = Depends(get_current_user)):
+    return await auth_service.sign_out(db, user_id)
+
+@router.get("/refresh",status_code=status.HTTP_200_OK)
+async def refresh_token(token:str = Depends(get_refresh)):
+    return {"access":token}
