60e0db1d3de2450d6016ceff8a03d7b9a3dacb03
Amal k <119653788+Amalkxx02@users.noreply.github.com>
2026-02-15 16:58:44 +0530

    security packages

---
+++ app/security/hashing.py
@@ -0,0 +1,19 @@
+import bcrypt
+import hashlib
+
+
+
+def hash_password(pwd: str) -> bytes:
+    return bcrypt.hashpw(pwd.encode(),bcrypt.gensalt())
+
+
+def verify_password(plain_pwd: str, hashed_pwd: bytes) -> bool:
+    return bcrypt.checkpw(plain_pwd.encode(), hashed_pwd)
+
+
+def get_fingerprint(value: str) -> bytes:
+    return hashlib.sha256(value.encode()).hexdigest().encode()
+
+
+def verify_fingerprint(value: str, fingerprint: bytes) -> bool:
+    return get_fingerprint(value) == fingerprint
