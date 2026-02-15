2bd2e91546188e13c652087473ff5369e8eea328
Amal k <119653788+Amalkxx02@users.noreply.github.com>
2026-02-15 21:09:02 +0530

    structural change

--- app/database/session.py
+++ app/database/session.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession,async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.core.config import settings
 from app.database.base import Base
 
