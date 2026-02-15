ce265dfb0c9604f5b42130a93d8e549d5c2f17d8
Amal k <119653788+Amalkxx02@users.noreply.github.com>
2026-02-15 22:50:55 +0530

    model update

--- app/models/device.py
+++ app/models/device.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, ForeignKey, String, UniqueConstraint
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
@@ -9,7 +9,7 @@ from app.database.base import Base
 
 class Device(Base):
     __tablename__ = "Devices"
-    device_id = Column(
+    id = Column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4,
@@ -20,14 +20,17 @@ class Device(Base):
         UUID(as_uuid=True),
         ForeignKey("Users.user_id", ondelete="CASCADE"),
         nullable=False,
+        index=True
     )
-    device_name = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    active = Column(Boolean,default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    last_seen = Column(DateTime(timezone=True), onupdate=func.now())
 
     __table_args__ = (
         UniqueConstraint(
-            "user_id", "device_name", name="unique_device_name_for_each_user"
+            "user_id", "name", name="unique_device_name_for_each_user"
         ),
     )
-    things = relationship("Thing", back_populates="device")
-    user = relationship("User", back_populates="devices")
+    # things = relationship("Thing", back_populates="device")
+    # user = relationship("User", back_populates="devices")
