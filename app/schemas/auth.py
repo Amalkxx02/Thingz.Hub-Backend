59d2bcacde14cbb669c43335a2aebb4aa987a378
Amal k <119653788+Amalkxx02@users.noreply.github.com>
2026-06-06 17:38:18 +0530

    Replace password validation with StrongPassword type

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel, EmailStr, AfterValidator, HttpUrl
 from typing import Annotated
 
-from .utils import email_formalize, is_strong_password
+from .utils import email_formalize, is_strong_password, StrongPassword
 
 
 class AuthIn(BaseModel):
     email: Annotated[EmailStr, AfterValidator(email_formalize)]
-    password: Annotated[str, AfterValidator(is_strong_password)]
+    password: StrongPassword
 
 class VerificationResponse(BaseModel):
     link: HttpUrl
